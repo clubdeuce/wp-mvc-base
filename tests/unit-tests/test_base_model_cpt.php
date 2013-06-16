a8b885b66e279e986e68c8691b59d957f90fb8f8
Daryl Lozupone <daryl@actionhook.com>
2013-06-16 08:12:00 -0400

    Added: namespacing
    Since phpUnit requires PHP 5.3, namespacing was added to testing classes.

--- tests/unit-tests/test_base_model_cpt.php
+++ tests/unit-tests/test_base_model_cpt.php
@@ -1,55 +1,59 @@
 <?php
-require_once( dirname( __FILE__ ) . '../../../models/base_model_cpt.php' );
-
-/**
- * The Test Stub CPT Model
- *
- * @package pkgtoken
- * @since WP MVC Base Testing 0.1
- */
-class Test_Stub_Base_Model_CPT extends Base_Model_CPT
+namespace WPMVCBase\Testing
 {
-	protected $slug = 'my-super-cool-cpt';
-	protected $metakey = '_my-super-cool-metakey';
-	protected $shortcodes = array(
-		'my-super-cool-shortcode' => 'my-super-cool-callback'
-	);
+	require_once( dirname( __FILE__ ) . '../../../models/base_model_cpt.php' );
 	
-	public function save( $postdata )
+	/**
+	 * The Test Stub CPT Model
+	 *
+	 * @package pkgtoken
+	 * @since WP MVC Base Testing 0.1
+	 */
+	class Test_Stub_Base_Model_CPT extends \Base_Model_CPT
 	{
-		//dummy implementation of abstract function
+		protected $slug = 'my-super-cool-cpt';
+		protected $metakey = '_my-super-cool-metakey';
+		protected $shortcodes = array(
+			'my-super-cool-shortcode' => 'my-super-cool-callback'
+		);
+		
+		public function save( $postdata )
+		{
+			//dummy implementation of abstract function
+		}
+	}
+	
+	/**
+	 * The test controller for Base_Model_CPT
+	 *
+	 * @package WPMVCBase_Testing\Unit_Tests
+	 * @since WPMVCBase 0.1
+	 * @internal
+	 */
+	 
+	class Test_Base_Model_CPT extends \WP_UnitTestCase
+	{
+		private $_cpt;
+		
+		public function SetUp()
+		{
+			$this->_cpt = new Test_Stub_Base_Model_CPT( 'http://example.com', 'my-super-cool-text-domain' );
+		}
+		
+		public function test_get_slug()
+		{
+			$this->assertEquals( 'my-super-cool-cpt', $this->_cpt->get_slug() );
+		}
+		
+		public function test_get_metakey()
+		{
+			$this->assertEquals( '_my-super-cool-metakey', $this->_cpt->get_metakey() );
+		}
+		
+		public function test_get_shortcodes()
+		{
+			$this->assertEquals( array( 'my-super-cool-shortcode' => 'my-super-cool-callback' ), $this->_cpt->get_shortcodes() );
+		}
 	}
 }
-
-/**
- * The test controller for Base_Model_CPT
- *
- * @package WPMVCBase_Testing\Unit_Tests
- * @since WPMVCBase 0.1
- * @internal
- */
- 
-class Test_Base_Model_CPT extends WP_UnitTestCase
-{
-	private $_cpt;
-	
-	public function SetUp()
-	{
-		$this->_cpt = new Test_Stub_Base_Model_CPT( 'http://example.com', 'my-super-cool-text-domain' );
-	}
-	
-	public function test_get_slug()
-	{
-		$this->assertEquals( 'my-super-cool-cpt', $this->_cpt->get_slug() );
-	}
-	
-	public function test_get_metakey()
-	{
-		$this->assertEquals( '_my-super-cool-metakey', $this->_cpt->get_metakey() );
-	}
-	
-	public function test_get_shortcodes()
-	{
-		$this->assertEquals( array( 'my-super-cool-shortcode' => 'my-super-cool-callback' ), $this->_cpt->get_shortcodes() );
-	}
-}
+?>
--- tests/unit-tests/test_base_model_metabox.php
+++ tests/unit-tests/test_base_model_metabox.php
@@ -1,107 +1,110 @@
 <?php
-require_once( dirname( __FILE__ ) . '../../../models/base_model_metabox.php' );
-/**
- * The test controller for Base_Model_Metabox.
- *
- * @package WPMVCBase_Testing\Unit_Tests
- * @since WPMVCBase 0.1
- * @internal
- */
- 
-class Test_Base_Model_Metabox extends WP_UnitTestCase
+namespace WPMVCBase\Testing
 {
-	public function SetUp()
-	{
-		$this->_metabox = new Base_Model_Metabox(
-			'my-super-cool-metabox',
-			'My Super Cool Metabox',
-			'my_super_cool_callback',
-			'my_super_cool_posttype',
-			'normal',
-			'default',
-			array( 'foo' => 'bar' )
-		);
-	}
-	
-	public function test_get_id()
-	{
-		$this->assertEquals( 'my-super-cool-metabox', $this->_metabox->get_id() );
-	}
-	
-	public function test_get_title()
-	{
-		$this->assertEquals( 'My Super Cool Metabox', $this->_metabox->get_title() );
-	}
-	
-	public function test_get_callback()
-	{
-		$this->assertEquals( 'my_super_cool_callback', $this->_metabox->get_callback() );
-	}
-	
-	public function test_get_post_type()
-	{
-		$this->assertEquals( 'my_super_cool_posttype', $this->_metabox->get_post_type() );
-	}
-	
-	public function test_get_context()
-	{
-		$this->assertEquals( 'normal', $this->_metabox->get_context() );
-	}
-	
-	public function test_get_priority()
-	{
-		$this->assertEquals( 'default', $this->_metabox->get_priority() );
-	}
-	
-	public function test_get_callback_args()
-	{
-		$this->assertEquals( array( 'foo' => 'bar' ), $this->_metabox->get_callback_args() );
-	}
-	
+	require_once( dirname( __FILE__ ) . '../../../models/base_model_metabox.php' );
 	/**
-	 * Test faulty metabox context assignment.
+	 * The test controller for Base_Model_Metabox.
 	 *
-	 * This function tests the metabox object's setting the context to 'normal' when
-	 * passed a value other than 'normal', 'advanced', or 'side'.
-	 *
-	 * @since 0.1
+	 * @package WPMVCBase_Testing\Unit_Tests
+	 * @since WPMVCBase 0.1
+	 * @internal
 	 */
-	public function test_invalid_context()
+	 
+	class Test_Base_Model_Metabox extends \WP_UnitTestCase
 	{
-		$metabox = new Base_Model_Metabox(
-			'my-super-cool-metabox',
-			'My Super Cool Metabox',
-			'my_super_cool_callback',
-			'my_super_cool_posttype',
-			'flibbertygibbet',
-			'default',
-			array( 'foo' => 'bar' )
-		);
+		public function SetUp()
+		{
+			$this->_metabox = new \Base_Model_Metabox(
+				'my-super-cool-metabox',
+				'My Super Cool Metabox',
+				'my_super_cool_callback',
+				'my_super_cool_posttype',
+				'normal',
+				'default',
+				array( 'foo' => 'bar' )
+			);
+		}
 		
-		$this->assertEquals( 'normal', $metabox->get_context() );
-	}
-	
-	/**
-	 * Test faulty metabox priority assignment.
-	 *
-	 * This function tests the metabox object's setting the priority to 'default' when
-	 * passed a value other than 'high', 'core', 'default', or 'low'.
-	 *
-	 * @since 0.1
-	 */
-	public function test_invalid_priority()
-	{
-		$metabox = new Base_Model_Metabox(
-			'my-super-cool-metabox',
-			'My Super Cool Metabox',
-			'my_super_cool_callback',
-			'my_super_cool_posttype',
-			'normal',
-			'flibbertygibbet',
-			array( 'foo' => 'bar' )
-		);
+		public function test_get_id()
+		{
+			$this->assertEquals( 'my-super-cool-metabox', $this->_metabox->get_id() );
+		}
 		
-		$this->assertEquals( 'default', $metabox->get_priority() );
+		public function test_get_title()
+		{
+			$this->assertEquals( 'My Super Cool Metabox', $this->_metabox->get_title() );
+		}
+		
+		public function test_get_callback()
+		{
+			$this->assertEquals( 'my_super_cool_callback', $this->_metabox->get_callback() );
+		}
+		
+		public function test_get_post_type()
+		{
+			$this->assertEquals( 'my_super_cool_posttype', $this->_metabox->get_post_type() );
+		}
+		
+		public function test_get_context()
+		{
+			$this->assertEquals( 'normal', $this->_metabox->get_context() );
+		}
+		
+		public function test_get_priority()
+		{
+			$this->assertEquals( 'default', $this->_metabox->get_priority() );
+		}
+		
+		public function test_get_callback_args()
+		{
+			$this->assertEquals( array( 'foo' => 'bar' ), $this->_metabox->get_callback_args() );
+		}
+		
+		/**
+		 * Test faulty metabox context assignment.
+		 *
+		 * This function tests the metabox object's setting the context to 'normal' when
+		 * passed a value other than 'normal', 'advanced', or 'side'.
+		 *
+		 * @since 0.1
+		 */
+		public function test_invalid_context()
+		{
+			$metabox = new \Base_Model_Metabox(
+				'my-super-cool-metabox',
+				'My Super Cool Metabox',
+				'my_super_cool_callback',
+				'my_super_cool_posttype',
+				'flibbertygibbet',
+				'default',
+				array( 'foo' => 'bar' )
+			);
+			
+			$this->assertEquals( 'normal', $metabox->get_context() );
+		}
+		
+		/**
+		 * Test faulty metabox priority assignment.
+		 *
+		 * This function tests the metabox object's setting the priority to 'default' when
+		 * passed a value other than 'high', 'core', 'default', or 'low'.
+		 *
+		 * @since 0.1
+		 */
+		public function test_invalid_priority()
+		{
+			$metabox = new \Base_Model_Metabox(
+				'my-super-cool-metabox',
+				'My Super Cool Metabox',
+				'my_super_cool_callback',
+				'my_super_cool_posttype',
+				'normal',
+				'flibbertygibbet',
+				array( 'foo' => 'bar' )
+			);
+			
+			$this->assertEquals( 'default', $metabox->get_priority() );
+		}
 	}
 }
 ?>
