195fac62152c93fedbb10ea673456cab2ba90ef1
Daryl Lozupone <daryl@actionhook.com>
2013-06-22 07:51:04 -0400

    Added: helper file

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -4,5 +4,5 @@ require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 
 require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
 
-
+require_once( dirname( __FILE__ ) . '/../helpers/base_helpers.php' );
 ?>
