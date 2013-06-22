9ea60e1b6a557a5d5c55ebaf7a486c28c36f12b2
Daryl Lozupone <daryl@actionhook.com>
2013-06-22 08:04:56 -0400

    Modified: base_model_cpt tests

--- tests/unit-tests/test_base_model_cpt.php
+++ tests/unit-tests/test_base_model_cpt.php
@@ -21,6 +21,78 @@ namespace WPMVCBase\Testing
 		{
 			parent::__construct( $uri, $txtdomain );
 			$this->help_tabs = array(  'title' => 'My Help Screen', 'id' => 'demo-help', 'call' => 'my_callback_function' );
+			$this->metaboxes = array(
+				'book_metabox' => array(
+					'id' => 'book_metabox',
+					'title' => __( 'Book Metabox', $txtdomain ),
+					'post_type' => 'my-super-cool-cpt',
+					'context' => 'normal',
+					'priority' => 'default',
+					'callback_args' => array () 
+				)
+			);
+			
+			$this->messages = array(
+				0 => null, // Unused. Messages start at index 1.
+				1 => sprintf( __('Book updated. <a href="%s">View book</a>', 'your_text_domain'), esc_url( get_permalink( $this->_post->ID) ) ),
+				2 => __('Custom field updated.', 'your_text_domain'),
+				3 => __('Custom field deleted.', 'your_text_domain'),
+				4 => __('Book updated.', 'your_text_domain'),
+				/* translators: %s: date and time of the revision */
+				5 => isset($_GET['revision']) ? sprintf( __('Book restored to revision from %s', 'your_text_domain'), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+				6 => sprintf( __('Book published. <a href="%s">View book</a>', 'your_text_domain'), esc_url( get_permalink($this->_post->ID) ) ),
+				7 => __('Book saved.', 'your_text_domain'),
+				8 => sprintf( __('Book submitted. <a target="_blank" href="%s">Preview book</a>', 'your_text_domain'), esc_url( add_query_arg( 'preview', 'true', get_permalink( $this->_post->ID) ) ) ),
+				9 => sprintf( __('Book scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview book</a>', 'your_text_domain'),
+				  // translators: Publish box date format, see http://php.net/date
+				  date_i18n( __( 'M j, Y @ G:i' ), strtotime( $this->_post->post_date ) ), esc_url( get_permalink($post_ID) ) ),
+				10 => sprintf( __('Book draft updated. <a target="_blank" href="%s">Preview book</a>', 'your_text_domain'), esc_url( add_query_arg( 'preview', 'true', get_permalink( $this->_post->ID) ) ) )
+			);
+			
+			$labels = array(
+				'name'                => _x( 'Books', 'Post Type General Name', 'my-super-cool-text-domain' ),
+				'singular_name'       => _x( 'Book', 'Post Type Singular Name', 'my-super-cool-text-domain' ),
+				'menu_name'           => __( 'Books', 'my-super-cool-text-domain' ),
+				'parent_item_colon'   => __( 'Parent Book', 'my-super-cool-text-domain' ),
+				'all_items'           => __( 'All Books', 'my-super-cool-text-domain' ),
+				'view_item'           => __( 'View Book', 'my-super-cool-text-domain' ),
+				'add_new_item'        => __( 'Add New Book', 'my-super-cool-text-domain' ),
+				'add_new'             => __( 'New Book', 'my-super-cool-text-domain' ),
+				'edit_item'           => __( 'Edit Book', 'my-super-cool-text-domain' ),
+				'update_item'         => __( 'Update Book', 'my-super-cool-text-domain' ),
+				'search_items'        => __( 'Search books', 'my-super-cool-text-domain' ),
+				'not_found'           => __( 'No books found', 'my-super-cool-text-domain' ),
+				'not_found_in_trash'  => __( 'No books found in Trash', 'my-super-cool-text-domain' ),
+			);
+
+			$this->args = array(
+				'description'         	=> __( 'Books', 'my-super-cool-text-domain' ),
+				'labels'              	=> $labels,
+				'supports'            	=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+				'hierarchical'        	=> false,
+				'public'              	=> true,
+				'show_ui'             	=> true,
+				'show_in_menu'        	=> true,
+				'show_in_nav_menus'   	=> true,
+				'show_in_admin_bar'   	=> true,
+				'menu_icon'           	=> null,
+				'can_export'          	=> true,
+				'has_archive'         	=> true,
+				'exclude_from_search' 	=> false,
+				'publicly_queryable'  	=> true,
+				'rewrite' 			  	=> array( 'slug' => 'books' ),
+				//this is supported in 3.6
+				'statuses'				=> array(
+					'draft' => array(
+						'label'                     => _x( 'New', 'book', 'my-super-cool-text-domain' ),
+						'public'                    => true,
+						'exclude_from_search'       => false,
+						'show_in_admin_all_list'    => true,
+						'show_in_admin_status_list' => true,
+						'label_count'               => _n_noop( 'New <span class="count">(%s)</span>', 'New <span class="count">(%s)</span>', 'my-super-cool-text-domain' )
+					)
+				)
+			);
 		}
 	}
 	
@@ -60,9 +132,15 @@ namespace WPMVCBase\Testing
 		
 		public function test_get_help_screen()
 		{
+			$this->setExpectedException( 'PHPUnit_Framework_Error' );
+			$this->_cpt->get_help_screen( __FILE__, 'my-super-cool-text-domain' );
+		}
+		
+		public function test_get_help_tabs()
+		{
 			$this->assertEquals( 
 				array( 'title' => 'My Help Screen', 'id' => 'demo-help', 'call' => 'my_callback_function' ),
-				$this->_cpt->get_help_screen( __FILE__, 'my-super-cool-text-domain' )
+				$this->_cpt->get_help_tabs( __FILE__, 'my-super-cool-text-domain' )
 			);
 		}
 		
@@ -95,7 +173,7 @@ namespace WPMVCBase\Testing
 		}
 		*/
 
-		public function test_init_metaboxes()
+		public function test_get_metaboxes()
 		{
 			$this->assertEquals(  
 				array(
@@ -134,7 +212,6 @@ namespace WPMVCBase\Testing
 				'description'         	=> __( 'Books', 'my-super-cool-text-domain' ),
 				'labels'              	=> $labels,
 				'supports'            	=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
-				'taxonomies'          	=> null,
 				'hierarchical'        	=> false,
 				'public'              	=> true,
 				'show_ui'             	=> true,
