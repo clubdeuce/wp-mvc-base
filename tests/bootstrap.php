14d19bc01970035dc48283c41348280f5f482571
Daryl Lozupone <daryl@actionhook.com>
2013-06-24 09:39:10 -0400

    Added: bootstrap messages

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -5,4 +5,7 @@ require_once getenv( 'WP_TESTS_DIR' ) . '/includes/functions.php';
 require getenv( 'WP_TESTS_DIR' ) . '/includes/bootstrap.php';
 
 require_once( dirname( __FILE__ ) . '/../helpers/base_helpers.php' );
+echo 'Welcome to the WP MVC Base Test Suite' . PHP_EOL;
+echo 'Version 1.0' . PHP_EOL;
+echo 'Author: Daryl Lozupone <daryl@actionhook.com>' . PHP_EOL;
 ?>
