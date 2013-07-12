6488df5273ea99b965b078669182d9eca07d051e
Daryl Lozupone <daryl@actionhook.com>
2013-07-12 00:21:15 -0400

    Implemented: getReflectionPropertyValue() Implemented: setReflectionPropertyValue() Fixed: inconsistent namespace

--- tests/framework/testcase.php
+++ tests/framework/testcase.php
@@ -1,12 +1,28 @@
 <?php
-namespace WPMVCBase\Testing
+namespace WPMVCB\Testing
 {
-	class WPMVBC_Test_Case extends \WP_UnitTestCase
-	{
+	class WPMVCB_Test_Case extends \WP_UnitTestCase
+	{		
 		public function __construct()
 		{
-			//parent::setUp();
+			parent::setUp(); 
 			$this->factory = new \WP_UnitTest_Factory;
+		}
+		
+		public function getReflectionPropertyValue( $class, $property )
+		{
+			$reflection = new \ReflectionClass( $class );
+			$property_reflection = $reflection->getProperty( $property );
+			$property_reflection->setAccessible( true );
+			return $property_reflection->getValue( $class );
+		}
+		
+		public function setReflectionPropertyValue( $class, $property, $value )
+		{
+			$reflection = new \ReflectionClass( $class );
+			$property_reflection = $reflection->getProperty( $property );
+			$property_reflection->setAccessible( true );
+			return $property_reflection->setValue( $class, $value );
 		}
 	}
 }
