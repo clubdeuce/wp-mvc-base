1fa560bc6644af34b4d3d7a9f2545ce524427edc
Daryl Lozupone <daryl@actionhook.com>
2013-07-15 14:14:51 -0400

    Refactored: moved metaboxes, help_tabs, and short codes properties and methods to Base_Model

--- models/base_model_cpt.php
+++ models/base_model_cpt.php
@@ -14,28 +14,27 @@ if ( ! class_exists( 'Base_Model_CPT' ) && class_exists( 'Base_Model' ) ):
 	 *
 	 * @package WPMVCBase\Models
 	 * @version 0.2
-	 * @abstract
 	 * @since WPMVCBase 0.1
 	 */
-	 abstract class Base_Model_CPT extends Base_Model
+	 class Base_Model_CPT extends Base_Model
 	 {
 	 	/**
-		 * The cpt slug.
+		 * The cpt slug. Will be used to register the post type.
 		 *
 		 * @var string
 		 * @access protected
 		 * @since 0.1
 		 */
-		protected $slug;
+		protected $_slug;
 		
 		/**
-		 * The cpt metakey .
+		 * The cpt metakey.
 		 *
 		 * @var array
 		 * @access protected
 		 * @since 0.1
 		 */
-		protected $metakey;
+		protected $_metakey;
 		
 		/**
 		 * The arguments passed to register_post_type.
@@ -45,7 +44,7 @@ if ( ! class_exists( 'Base_Model_CPT' ) && class_exists( 'Base_Model' ) ):
 		 * @since 0.1
 		 * @link http://codex.wordpress.org/Function_Reference/register_post_type#Arguments
 		 */
-		protected $args;
+		protected $_args;
 		
 		/**
 		 * The CPT post updated/deleted/etc messages.
@@ -55,7 +54,7 @@ if ( ! class_exists( 'Base_Model_CPT' ) && class_exists( 'Base_Model' ) ):
 		 * @since 0.1
 		 * @link http://codex.wordpress.org/Function_Reference/register_post_type
 		 */
-		protected $messages;
+		protected $_messages;
 		
 		/**
 		 * Shortcodes exposed by this CPT.
@@ -70,192 +69,189 @@ if ( ! class_exists( 'Base_Model_CPT' ) && class_exists( 'Base_Model' ) ):
 		 * @access protected
 		 * @since 0.1
 		 */
-		protected $shortcodes;
+		protected $_shortcodes;
 		
 		/**
 		 * The Help screen tabs for this CPT
 		 *
-		 * This property is an array containing individual help screen definitions.
-		 * Example:
-		 * <code>
-		 * $help_tabs = array(  'title' => __( 'My Help Screen', 'my_text_domain' ), 'id' => 'demo-help', 'call' => 'my_callback_function' );
-		 * </code>
+		 * This property is an array containing individual help screen object.
+		 *
 		 * @var array
 		 * @access protected
 		 * @since 0.1
+		 * @see Base_Model_Help_Tab
 		 */
-		protected $help_tabs;
+		protected $_help_tabs;
 		
 		/**
 		 * The class constructor.
 		 *
 		 * Use this function to initialize class variables, require necessary files, etc.
 		 *
+		 * @param string $slug The post type slug.
+		 * @param string $singular The singular post type name in lower case (e.g. Book).
+		 * @param string $plural The plural post type name in lower case (e.g. Books).
 		 * @param string $uri The plugin uri.
 		 * @param string $txtdomain The plugin text domain.
 		 * @return void
 		 * @access public
 		 * @since 0.1
 		 */
-		public function __construct( $uri = '', $txtdomain = '' )
+		public function __construct( $slug, $singular, $plural, $uri = '', $txtdomain = '' )
 		{
-			$this->txtdomain = $txtdomain;
+			$this->_slug = $slug;
+			$this->_singular = $singular;
+			$this->_plural = $plural;
+			$this->_uri = $uri;
+			$this->_txtdomain = $txtdomain;
 			
-			if ( method_exists( $this, 'init' ) )
-				$this->init( $uri, $txtdomain );
+			$this->_init_labels();
 		}
-		
+		/**
+		 */
+		protected function _init_labels()
+		{
+			$this->_labels = array(
+				'name'                => $this->_plural,
+				'singular_name'       => $this->_singular,
+				'menu_name'           => $this->_plural,
+				'parent_item_colon'   => sprintf( __( 'Parent %s', $this->_txtdomain ), $this->_singular ),
+				'all_items'           => sprintf( __( 'All %s', $this->_txtdomain ), $this->_plural ),
+				'view_item'           => sprintf( __( 'View %s', $this->_txtdomain ), $this->_singular ),
+				'add_new_item'        => sprintf( __( 'Add New %s', $this->_txtdomain ), $this->_singular ),
+				'add_new'             => sprintf( __( 'New %s', $this->_txtdomain ), $this->_singular ),
+				'edit_item'           => sprintf( __( 'Edit %s', $this->_txtdomain ), $this->_singular ),
+				'update_item'         => sprintf( __( 'Update %s', $this->_txtdomain ), $this->_singular ),
+				'search_items'        => sprintf( __( 'Search %s', $this->_txtdomain ), $this->_plural ),
+				'not_found'           => sprintf( __( 'No %s found', $this->_txtdomain ), strtolower( $this->_plural ) ),
+				'not_found_in_trash'  => sprintf( __( 'No %s found in Trash', $this->_txtdomain ), strtolower( $this->_plural ) ),
+			);
+		}
 		
 		/**
 		 * Get the CPT messages
 		 *
+		 * @param string $this->_singular The singular CPT post name (e.g. Book).
+		 * @param string $this->_plural The plural CPT post name (e.g. Books).
 		 * @param object $post The WP post object.
-		 * @param string $txtdomain The text domain to use for localization.
-		 * @return array $messages The messages array.
+		 * @param string $this->_txtdomain The text domain to use for localization.
+		 * @return array $_messages The messages array.
 		 * @access public
 		 * @since 0.1
 		 */
-		public function get_post_updated_messages( $post, $txtdomain ) 
+		public function get_post_updated_messages( $post ) 
 		{
-			if ( ! isset( $this->messages ) && method_exists( $this, 'init_messages' ) )
-				$this->init_messages( $post, $this->txtdomain );
-				
-			if( ! isset( $this->messages ) )
-				trigger_error( 
-					sprintf( __( 'CPT messages are not set for %s', $this->txtdomain ), get_class( $this ) ),
-					E_USER_WARNING 
-				);
+			$messages = array(
+				0 => null, // Unused. Messages start at index 1.
+				1 => sprintf(
+					__('%1$s updated. <a href="%3$s">View %2$s</a>', $this->_txtdomain),
+					$this->_singular,
+					strtolower( $this->_singular ),
+					esc_url( get_permalink( $post->ID) )
+				),
+				2 => __('Custom field updated.', $this->_txtdomain),
+				3 => __('Custom field deleted.', $this->_txtdomain),
+				4 => sprintf( __('%s updated.', $this->_txtdomain), $this->_singular ),
+				/* translators: %2$s: date and time of the revision */
+				5 => isset($_GET['revision']) ? sprintf( __('%1$s restored to revision from %s', $this->_txtdomain), $this->_singular, wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+				6 => sprintf( __('%s published. <a href="%s">View book</a>', $this->_txtdomain), $this->_singular, esc_url( get_permalink($post->ID) ) ),
+				7 => sprintf( __('%s saved.', $this->_txtdomain), $this->_singular ),
+				8 => sprintf( 
+					__('%1$s submitted. <a target="_blank" href="%3$s">Preview %2$s</a>', $this->_txtdomain),
+					$this->_singular,
+					strtolower( $this->_singular ), 
+					esc_url( add_query_arg( 'preview', 'true', get_permalink( $post->ID) ) )
+				),
+				9 => sprintf(
+					__('%3$s scheduled for: <strong>%1$s</strong>. <a target="_blank" href="%2$s">Preview %4$s</a>', $this->_txtdomain), 
+					// translators: Publish box date format, see http://php.net/date
+					date_i18n( __( 'M j, Y @ G:i' ), strtotime( $this->_post->post_date ) ),
+					esc_url( get_permalink($post->ID) ),
+					$this->_singular,
+					strtolower( $this->_singular )
+				),
+				10 => sprintf(
+					__('%1$s draft updated. <a target="_blank" href="%3$s">Preview %2$s</a>', $this->_txtdomain),
+					$this->_singular,
+					strtolower( $this->_singular ),
+					esc_url( add_query_arg( 'preview', 'true', get_permalink( $post->ID) ) ) )
+			);
 			
-			return $this->messages;
+			return $messages;
 		}
 		
 		/**
 		 * get the cpt slug
 		 *
-		 * @return string $slug
+		 * @return string $_slug
 		 * @access public
 		 * @since 0.1
 		 */
 		public function get_slug()
 		{
-			if ( !isset( $this->slug ) || $this->slug == '' )
-				trigger_error( 
-					sprintf( __( 'CPT Slug is not set for', $this->txtdomain ), get_class( $this ) ),
-					E_USER_WARNING
-				);
-
-			return $this->slug;
+			return $this->_slug;
 		}
 		
 		/**
 		 * Get the cpt arguments.
 		 *
-		 * @param string $txtdomain The plugin text domain.
-		 * @return array $args
+		 * @param string $this->_txtdomain The plugin text domain.
+		 * @return array $_args
 		 * @access public
 		 * @since 0.1
 		 */
-		public function get_args( $txtdomain )
+		public function get_args()
 		{				
-			if( ! isset( $this->args ) ):
-				if ( ! method_exists( $this, 'init_args' ) ):
-					trigger_error(
-						sprintf(
-							__( 'Arguments for %s post type not set', $this->txtdomain ),
-							$this->slug
-						),
-						E_USER_WARNING
-					);
-				else:
-					$this->init_args( $txtdomain );
-				endif;
+			if( isset( $this->_args ) ):
+				return $this->_args;
+			else:
+				trigger_error( sprintf( __( 'Arguments for %s post type not set', $this->_txtdomain ), $this->_slug ), E_USER_WARNING );
 			endif;
-			
-			return $this->args;
-		}
-		
-		/**
-		 * Get the cpt shortcodes.
-		 *
-		 * @return array $shortcodes
-		 * @access public
-		 * @since 0.1
-		 */
-		public function get_shortcodes()
-		{				
-			if( ! isset( $this->shortcodes ) && method_exists( $this, 'init_shortcodes' ) )
-				$this->init_shortcodes();
-			
-			return $this->shortcodes;
-		}
-		
-		/**
-		 * Get the cpt help screen tabs.
-		 *
-		 * @param string $path The plugin app views path.
-		 * @param string $txtdomain The plugin text domain.
-		 * @return array $help_tabs Contains the help screen tab objects.
-		 * @access public
-		 * @since 0.1
-		 */
-		public function get_help_tabs( $path, $txtdomain )
-		{
-			if( ! isset( $this->help_tabs ) && method_exists( $this, 'init_help_tabs' ) )
-				$this->init_help_tabs( $path, $txtdomain );
-			
-			return $this->help_tabs;
-		}
-		
-		/**
-		 * Get the cpt help screen tabs.
-		 *
-		 * @param string $path The plugin app views path.
-		 * @param string $txtdomain The plugin text domain.
-		 * @return array $help_tabs Contains the help screen tab objects.
-		 * @access public
-		 * @deprecated
-		 * @since 0.1
-		 */
-		public function get_help_screen( $path, $txtdomain )
-		{
-			//warn the user about deprecated function use
-			Helper_Functions::deprecated( __FUNCTION__, 'get_help_tabs', $this->txtdomain );
-			
-			//and point to the replacement function
-			return $this->get_help_tabs( $path, $txtdomain );
 		}
 		
 		/**
 		 * Get the cpt metakey.
 		 *
-		 * @return string $metakey
+		 * @return string $_metakey
 		 * @return void
 		 * @access public
 		 * @since 0.1
 		 */
 		public function get_metakey()
 		{
-			if( ! isset( $this->metakey ) )
+			if( ! isset( $this->_metakey ) )
 				trigger_error( 
-					sprintf( __( 'Metakey is not set for %s', $this->txtdomain ), get_class( $this ) ),
+					sprintf( __( 'Metakey is not set for %s', $this->_txtdomain ), $this->_slug ),
 					E_USER_WARNING
 				);
 			
-			return $this->metakey;
+			return $this->_metakey;
 		}
 		
 		/**
 		 * Register this post type.
 		 *
-		 * @param string $uri The plugin uri.
-		 * @param string $txtdomain The plugin text domain.
 		 * @return object The registered post type object on success, WP_Error object on failure
 		 * @access public
 		 * @since 0.1
 		 */
 		public function register()
-		{	
-			return register_post_type( $this->slug, $this->get_args( $this->txtdomain ) );
+		{
+			return register_post_type( $this->_slug, $this->_args );	
+		}
+		
+		public function set_metakey( $metakey )
+		{
+			$this->_metakey = $metakey;
+		}
+		
+		public function set_args( $args )
+		{
+			if ( is_array( $args ) ):
+				$this->_args = $args;
+			else:
+				trigger_error( sprintf( __( '%s expects an array', 'wpmvcb' ), __FUNCTION__ ), E_USER_WARNING );
+			endif;
 		}
 	 }
 endif;
