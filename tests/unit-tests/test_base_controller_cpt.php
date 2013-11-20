5ff43bc504550f767b2048b67d5aaec55501d267
Daryl Lozupone <daryl@actionhook.com>
2013-11-20 16:30:05 -0500

    Added: require the base controller class

--- tests/unit-tests/test_base_controller_cpt.php
+++ tests/unit-tests/test_base_controller_cpt.php
@@ -1,6 +1,7 @@
 <?php
 namespace WPMVCB\Testing
 {
+	require_once( WPMVCB_SRC_DIR . '/controllers/base_controller.php' );
 	require_once( WPMVCB_SRC_DIR . '/controllers/base_controller_cpt.php' );
 	
 	/**
