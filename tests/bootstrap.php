e17ba78566e06b34aa2ba7b40bd4a7138ad16911
Daryl Lozupone <daryl@actionhook.com>
2013-11-20 01:07:09 -0500

    Removed: base helper class inclusion

--- tests/bootstrap.php
+++ tests/bootstrap.php
@@ -20,7 +20,6 @@ require_once( WPMVCB_TEST_DIR . '/includes/vfs/vfsStreamDirectory.php' );
 require_once( WPMVCB_TEST_DIR . '/includes/vfs/vfsStreamWrapper.php' );
 
 require_once( 'framework/testcase.php' );
-require_once( WPMVCB_SRC_DIR . '/helpers/base_helpers.php' );
 
 echo 'Welcome to the WP MVC Base Test Suite' . PHP_EOL;
 echo 'Version 1.0' . PHP_EOL;
