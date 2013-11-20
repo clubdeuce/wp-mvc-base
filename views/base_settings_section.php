f78254fccb1a26c7074ded1bf8c160dbbe202fb8
Daryl Lozupone <daryl@actionhook.com>
2013-11-19 22:45:24 -0500

    Updated: base views

--- views/base_settings_section.php
+++ views/base_settings_section.php
@@ -7,8 +7,7 @@
  * @version 0.1
  * @since WP Base 0.1
  */
-?>
-<?php 
-if ( isset( $setting_section['content'] ) && $setting_section['content'] != '' )
-	echo $setting_section['content'];
-?>
+
+if ( isset( $setting_section['content'] ) && $setting_section['content'] != '' ) {
+    echo $setting_section['content'];
+}
