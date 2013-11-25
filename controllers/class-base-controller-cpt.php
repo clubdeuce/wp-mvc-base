74748fc5e762ecefe8e8e600dbd59ce5badb650d
Daryl Lozupone <daryl@actionhook.com>
2013-11-24 20:57:45 -0500

    Modified: add_meta_boxes() to support array for post types

--- controllers/class-base-controller-cpt.php
+++ controllers/class-base-controller-cpt.php
@@ -127,15 +127,17 @@ if ( ! class_exists( 'Base_Controller_CPT' ) && class_exists( 'Base_Controller' 
 				foreach ( $this->_cpt_models as $cpt ) {
 					if ( $metaboxes = $cpt->get_metaboxes( $post, $this->txtdomain ) ) {
 						foreach ( $metaboxes as $metabox ) {
-							add_meta_box( 
-								$metabox->get_id(),
-								$metabox->get_title(),
-								$metabox->get_callback(),
-								$metabox->get_post_type(),
-								$metabox->get_context(),
-								$metabox->get_priority(),
-								$metabox->get_callback_args()
-							);
+							foreach( $metabox->get_post_types() as $post_type ) {
+								add_meta_box( 
+									$metabox->get_id(),
+									$metabox->get_title(),
+									$metabox->get_callback(),
+									$post_type,
+									$metabox->get_context(),
+									$metabox->get_priority(),
+									$metabox->get_callback_args()
+								);
+							}
 						}
 					}
 				}
