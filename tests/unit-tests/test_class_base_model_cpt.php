7f4bbf5f16cd7ae4733355f2293f5f1d42af81de
Daryl Lozupone <daryl@actionhook.com>
2013-11-28 14:48:19 -0500

    Modified testMethodGetArgsError() Renamed to testMethodGetArgsDefaults Removed dependency upon testMethodGetArgs()

--- tests/unit-tests/test_class_base_model_cpt.php
+++ tests/unit-tests/test_class_base_model_cpt.php
@@ -169,13 +169,47 @@ namespace WPMVCB\Testing
 		}
 
 		/**
+		 * Test the return of default arguments
+		 *
 		 * @covers Base_Model_CPT::get_args
-		 * @depends testMethodGetArgs
 		 */
-		public function testMethodGetArgsError()
+		public function testMethodGetArgsDefaults()
 		{
-			$this->setExpectedException( 'PHPUnit_Framework_Error', 'Arguments for fooslug post type not set' );
-			$this->_cpt->get_args();
+			$this->assertTrue( method_exists( $this->_cpt, 'get_args' ) );
+			
+			$expected = array(
+				'description'         	=> 'Books',
+				'labels'              	=> array(
+								                'name' => 'Books',
+												'singular_name' => 'Book',
+												'menu_name' => 'Books',
+												'parent_item_colon' => 'Parent Book',
+												'all_items' => 'All Books',
+												'view_item' => 'View Book',
+												'add_new_item' => 'Add New Book',
+												'add_new' => 'New Book',
+												'edit_item' => 'Edit Book',
+												'update_item' => 'Update Book',
+												'search_items' => 'Search Books',
+												'not_found' => 'No books found',
+												'not_found_in_trash' => 'No books found in Trash'
+											),
+				'supports'            	=> array( 'title', 'editor', 'author', 'thumbnail', 'excerpt', 'comments' ),
+				'hierarchical'        	=> false,
+				'public'              	=> true,
+				'show_ui'             	=> true,
+				'show_in_menu'        	=> true,
+				'show_in_nav_menus'   	=> true,
+				'show_in_admin_bar'   	=> true,
+				'menu_icon'           	=> null,
+				'can_export'          	=> true,
+				'has_archive'         	=> true,
+				'exclude_from_search' 	=> false,
+				'publicly_queryable'  	=> true,
+				'rewrite' 			  	=> array( 'slug' => 'fooslug' ),
+			);
+			
+			$this->assertSame( $expected, $this->_cpt->get_args() );
 		}
 	}
 } //namespace
