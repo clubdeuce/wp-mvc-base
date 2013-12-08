51e4d34acddb1689aa9fb3ac7cb2030949a6dd42
Daryl Lozupone <daryl@actionhook.com>
2013-12-07 19:14:42 -0500

    Update Base_Model_Help_Tab removed $callback and $view properties removed associated getters and setters updated unit tests

--- models/class-base-model-help-tab.php
+++ models/class-base-model-help-tab.php
@@ -75,41 +75,14 @@ if ( ! class_exists( 'Base_Model_Help_Tab' ) ):
 		 * @param string       $id        The help tab id.
 		 * @param array        $screens   The screens on which to add the help screen tab.
 		 * @param string       $content   The help tab content. If null, the callback will be used to populate the content.
-		 * @param string|array $callback  The help tab callback. If null, the view will be used to populate the content.
-		 * @param string       $view      The absolute path to the view file used to render this help tab. Must be set if $content and $callback are null.
 		 * @since 0.1
 		 */
-		public function __construct( $title, $id, array $screens, $content = null, $callback = null, $view = null )
+		public function __construct( $title, $id, array $screens, $content )
 		{
-			if ( ! isset( $content ) && ! isset( $callback ) && ! isset( $view ) ) {
-				trigger_error(
-					'You must specify either the help tab content, a callback function, or a view to use for the help tab',
-					E_USER_WARNING
-				);
-			}
-			
 			$this->id       = $id;
 			$this->title    = $title;
 			$this->screens  = $screens;
 			$this->content  = $content;
-			$this->callback = $callback;
-			$this->view     = $view;
-		}
-
-		/**
-		 * Add this help tab to the current screen.
-		 *
-		 * @link http://codex.wordpress.org/Function_Reference/add_help_tab
-		 * @since 0.1
-		 */
-		public function add()
-		{
-			if ( is_null( $this->callback ) && file_exists( $this->view ) ) {
-				ob_start();
-				require_once( $this->view );
-				$this->content = ob_get_clean();
-			}
-			get_current_screen()->add_help_tab( array( 'id' => $this->id, 'title' => $this->title, 'content' => $this->content ) );
 		}
 
 		/**
@@ -146,38 +119,21 @@ if ( ! class_exists( 'Base_Model_Help_Tab' ) ):
 			
 			return false;
 		}
-		
-		/**
-		 * Set the help tab callback.
-		 *
-		 * @param string|array $callback The help tab callback.
-		 * @since 0.1
-		 */
-		public function set_callback( $callback )
-		{
-			if ( ! is_callable( $callback ) ):
-				trigger_error(
-					__( 'A valid callback function must be specified', 'wp-mvc-base' ),
-					E_USER_WARNING
-				);
-
-				return false;
-			endif;
-
-			$this->callback = $callback;
-
-			return true;
-		}
 
 		/**
 		 * Set the help tab content.
 		 *
-		 * @param string $content The help tab content.
+		 * @return string $content
 		 * @since 0.1
 		 */
-		public function set_content( $content )
+		
+		public function get_content()
 		{
-			$this->content = $content;
+			if( isset( $this->content ) ) {
+				return	$content;
+			}
+			
+			return false;
 		}
 	}
 endif;
--- tests/unit-tests/test_base_model_help_tab.php
+++ tests/unit-tests/test_base_model_help_tab.php
@@ -14,34 +14,18 @@ namespace WPMVCB\Testing
 				'My Test Tab',
 				'test-tab',
 				array( 'load-post-new.php' ),
-				'Here is some test tab content',
-				array( &$this, 'help_tab_callback' )
+				'Here is some test tab content'
 			);
 		}
 		
-		public function mock_callback()
-		{
-			//implemented, but does nothing
-		}
-		
 		/**
 		 * @expectedException        PHPUnit_Framework_Error
-		 * @expectedExceptionMessage You must specify either the help tab content, a callback function, or a view to use for the help tab
 		 */
-		public function testMissingParamsError()
+		public function testPagesNotArray()
 		{
-			$tab = new \Base_Model_Help_Tab( 'Empty Tab', 'empty-tab', array( 'foo.php' ) );
+			$tab = new \Base_Model_Help_Tab( 'Empty Tab', 'empty-tab', 'foo.php' );
 		}
-		
-		/**
-		 * @expectedException        PHPUnit_Framework_Error
-		 * @expectedExceptionMessage You must specify either the help tab content, a callback function, or a view to use for the help tab
-		 */
-		public function testMissingParamsMessage()
-		{
-			$tab = new \Base_Model_Help_Tab( 'Empty Tab', 'empty-tab', array( 'foo.php' ) );
-		}
-		
+				
 		public function testAttributeIdExists()
 		{
 			$this->assertClassHasAttribute( 'id', 'Base_Model_Help_Tab' );
@@ -62,16 +46,6 @@ namespace WPMVCB\Testing
 			$this->assertClassHasAttribute( 'content', 'Base_Model_Help_Tab' );
 		}
 		
-		public function testAttributeCallbackExists()
-		{
-			$this->assertClassHasAttribute( 'callback', 'Base_Model_Help_Tab' );
-		}
-		
-		public function testAttributeViewExists()
-		{
-			$this->assertClassHasAttribute( 'view', 'Base_Model_Help_Tab' );
-		}
-		
 		public function testMethodGetId()
 		{
 			$this->assertTrue( method_exists( 'Base_Model_Help_Tab', 'get_id' ) );
@@ -90,50 +64,5 @@ namespace WPMVCB\Testing
 			$this->assertEquals( array( 'load-post-new.php' ), $this->tab->get_screens() );
 		}
 		
-		public function testMethodSetCallbackExists()
-		{
-			$this->assertTrue( method_exists( 'Base_Model_Help_Tab', 'set_callback' ) );
-		}
-		
-		/**
-		 * @depends testMethodSetCallbackExists
-		 */
-		public function testMethodSetCallbackError()
-		{
-			$this->setExpectedException( 'PHPUnit_Framework_Error' );
-			$this->tab->set_callback( 'foo' );
-		}
-		
-		/**
-		 * @depends testMethodSetCallbackExists
-		 */
-		public function testSetCallbackMessage()
-		{
-			@$this->tab->set_callback( 'foo' );
-			$error = error_get_last();
-			$this->assertEquals( 'A valid callback function must be specified', $error['message'] );
-		}
-		
-		/**
-		 * @depends testMethodSetCallbackExists
-		 */
-		public function testSetCallback()
-		{
-			$this->assertTrue( $this->tab->set_callback( array( &$this, 'mock_callback' ) ) );
-			$this->assertEquals( array( &$this, 'mock_callback' ), $this->getReflectionPropertyValue( $this->tab, 'callback' ) );
-		}
-		
-		public function testMethodSetContent()
-		{
-			$this->assertTrue( method_exists( 'Base_Model_Help_Tab', 'set_content' ) );
-			
-			$this->tab->set_content( 'foo' );
-			$this->assertEquals( 'foo', $this->getReflectionPropertyValue( $this->tab, 'content' ) );
-		}
-		
-		public function testAdd()
-		{
-			$this->markTestIncomplete( 'This test not yet implemented' );
-		}
 	}
 }
