a758c4f6c7070e18095268fddc2ee58cf14de559
Daryl Lozupone <daryl@clubduece.com>
2013-12-08 01:18:53 -0500

    Implemented Base_Model_Admin_Notice Added unit tests

---
+++ models/class-base-model-admin-notice.php
@@ -0,0 +1,96 @@
+<?php
+
+/**
+ * The base admin notice model.
+ *
+ * @author Daryl Lozupone <daryl@clubduece.com>
+ */
+
+if ( ! class_exists( 'Base_Model_Admin_Notice' ) ) {
+	/**
+	 * The base admin notice object model.
+	 *
+	 * @package WPMVCBase\Models
+	 * @version 0.3
+	 * @since WPMVCBase 0.3
+	 */
+	class Base_Model_Admin_Notice
+	{
+		/**
+		 * The notice type ( updated or error ).
+		 * 
+		 * @var string
+		 * @since 1.0
+		 */
+		private $type;
+		
+		/**
+		 * The admin notice message.
+		 * 
+		 * @var string
+		 * @since 1.0
+		 */
+		private $message;
+		
+		/**
+		 * The screens on which to display this admin notice.
+		 * 
+		 * @var array
+		 * @since 1.0
+		 */
+		private $screens = array();
+		
+		/**
+		 * The class constructor.
+		 *
+		 * @param string $type The notice type. Either 'updated' or 'error'.
+		 * @param string $message The notice message.
+		 * @param array $screens The WP screen id's on which to display this notice. Defaults to all screens.
+		 * @since 1.0
+		 */
+		public function __construct( $type, $message, array $screens = null )
+		{
+			$this->type    = $type;
+			$this->message = $message;
+			$this->screens = 'all';
+			
+			if ( isset( $screens ) ) {
+				$this->screens  = $screens;
+			}
+		}
+		
+		/**
+		 * Get the notice type.
+		 *
+		 * @return string $type Either 'updated' or 'error'.
+		 * @since 1.0
+		 */
+		public function get_type()
+		{
+			return $this->type;
+		}
+		
+		/**
+		 * Get the admin notice message.
+		 *
+		 * @return string $message
+		 * @since 1.0
+		 */
+		public function get_message()
+		{
+			return $this->message;
+		}
+		
+		/**
+		 * Get the screen(s) on which to apply this notice.
+		 *
+		 * @return array $screens
+		 * @since 1.0
+		 */
+		public function get_screens()
+		{
+			return $this->screens;
+		}
+	}
+}
+	 
---
+++ tests/unit-tests/testBaseModelAdminNotice.php
@@ -0,0 +1,77 @@
+<?php
+namespace WPMVCB\Testing
+{
+	require_once WPMVCB_SRC_DIR . '/models/class-base-model-admin-notice.php';
+
+	/**
+	 * The tests for Base_Model_Admin_Notice
+	 * @since WPMVCBase 0.1
+	 * @internal
+	 */
+
+	class testBaseModelAdminNotice extends WPMVCB_Test_Case
+	{
+		public function setUp()
+		{
+			$this->admin_notice = new \Base_Model_Admin_Notice( 'updated', 'foo message', array( 'post' ) );
+		}
+		
+		public function tearDown()
+		{
+			unset( $this->admin_notice );
+		}
+		
+		public function testPropertyExistsType()
+		{
+			$this->assertClassHasAttribute( 'type', 'Base_Model_Admin_Notice' );
+		}
+		
+		public function testPropertyExistsMessage()
+		{
+			$this->assertClassHasAttribute( 'message', 'Base_Model_Admin_Notice' );
+		}
+		
+		public function testPropertyExistsScreens()
+		{
+			$this->assertClassHasAttribute( 'screens', 'Base_Model_Admin_Notice' );
+		}
+		
+		/**
+		 * @covers Base_Model_Admin_Notice::get_type
+		 */
+		public function testMethodGetType()
+		{
+			$this->assertTrue( method_exists( 'Base_Model_Admin_Notice', 'get_type' ), 'Method get_type does not exist' );
+			$this->assertEquals( 'updated', $this->admin_notice->get_type() );
+		}
+		
+		/**
+		 * @covers Base_Model_Admin_Notice::get_message
+		 */
+		public function testMethodGetMessage()
+		{
+			$this->assertTrue( method_exists( 'Base_Model_Admin_Notice', 'get_message' ), 'Method get_message does not exist' );
+			$this->assertEquals( 'foo message', $this->admin_notice->get_message() );
+		}
+		
+		/**
+		 * @covers Base_Model_Admin_Notice::get_screens
+		 */
+		public function testMethodGetScreens()
+		{
+			$this->assertTrue( method_exists( 'Base_Model_Admin_Notice', 'get_screens' ), 'Method get_screens does not exist' );
+			$this->assertEquals( array( 'post' ), $this->admin_notice->get_screens() );
+		}
+		
+		/**
+		 * @covers Base_Model_Admin_Notice::get_screens
+		 */
+		public function testMethodGetScreensEmptyScreens()
+		{
+			$this->assertTrue( method_exists( 'Base_Model_Admin_Notice', 'get_screens' ), 'Method get_screens does not exist' );
+			
+			$notice = new \Base_Model_Admin_Notice( 'error', 'Foo' );
+			$this->assertEquals( 'all', $notice->get_screens() );
+		}
+	}
+}
