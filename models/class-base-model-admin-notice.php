06eca9bce3566058f7aaa0c0ffee71500cdb0fbb
Daryl Lozupone <daryl@clubduece.com>
2013-12-08 11:24:49 -0500

    Updated comments for Base_Model_Admin_Notice

--- models/class-base-model-admin-notice.php
+++ models/class-base-model-admin-notice.php
@@ -1,52 +1,65 @@
 <?php
+ /*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
 
-/**
- * The base admin notice model.
- *
- * @author Daryl Lozupone <daryl@clubduece.com>
- */
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
 
 if ( ! class_exists( 'Base_Model_Admin_Notice' ) ) {
 	/**
 	 * The base admin notice object model.
 	 *
 	 * @package WPMVCBase\Models
-	 * @version 0.3
-	 * @since WPMVCBase 0.3
+	 * @version 0.2
+	 * @since   WPMVCBase 0.2
 	 */
 	class Base_Model_Admin_Notice
 	{
 		/**
 		 * The notice type ( updated or error ).
 		 * 
-		 * @var string
-		 * @since 1.0
+		 * @var    string
+		 * @access private
+		 * @since  WPMVCBase 0.2
 		 */
 		private $type;
 		
 		/**
 		 * The admin notice message.
 		 * 
-		 * @var string
-		 * @since 1.0
+		 * @var    string
+		 * @access private
+		 * @since  WPMVCBase 0.2
 		 */
 		private $message;
 		
 		/**
 		 * The screens on which to display this admin notice.
 		 * 
-		 * @var array
-		 * @since 1.0
+		 * @var    array
+		 * @access private
+		 * @since  WPMVCBase 0.2
 		 */
 		private $screens;
 		
 		/**
 		 * The class constructor.
 		 *
-		 * @param string $type The notice type. Either 'updated' or 'error'.
-		 * @param string $message The notice message.
-		 * @param array $screens The WP screen id's on which to display this notice. If not set, notice appears on all screens.
-		 * @since 1.0
+		 * @param  string $type The notice type. Either 'updated' or 'error'.
+		 * @param  string $message The notice message.
+		 * @param  array  $screens The WP screen id's on which to display this notice. If not set, notice appears on all screens.
+		 * @access public
+		 * @since  WPMVCBase 0.2
 		 */
 		public function __construct( $type, $message, array $screens = null )
 		{
@@ -63,7 +76,8 @@ if ( ! class_exists( 'Base_Model_Admin_Notice' ) ) {
 		 * Get the notice type.
 		 *
 		 * @return string $type Either 'updated' or 'error'.
-		 * @since 1.0
+		 * @access public
+		 * @since  WPMVCBase 0.2
 		 */
 		public function get_type()
 		{
@@ -74,7 +88,8 @@ if ( ! class_exists( 'Base_Model_Admin_Notice' ) ) {
 		 * Get the admin notice message.
 		 *
 		 * @return string $message
-		 * @since 1.0
+		 * @access public
+		 * @since  WPMVCBase 0.2
 		 */
 		public function get_message()
 		{
@@ -85,7 +100,8 @@ if ( ! class_exists( 'Base_Model_Admin_Notice' ) ) {
 		 * Get the screen(s) on which to apply this notice.
 		 *
 		 * @return array|string $screens An array of the screens or the string 'all' for all screens.
-		 * @since 1.0
+		 * @access public
+		 * @since  WPMVCBase 0.2
 		 */
 		public function get_screens()
 		{
@@ -93,4 +109,3 @@ if ( ! class_exists( 'Base_Model_Admin_Notice' ) ) {
 		}
 	}
 }
-	 
