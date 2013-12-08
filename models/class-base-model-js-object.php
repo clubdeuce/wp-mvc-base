43938f1215459a495b161f1c5c10d162f9f2e982
Daryl Lozupone <daryl@clubduece.com>
2013-12-08 14:11:16 -0500

    Updated comments for Base_Model_Js_Object

--- models/class-base-model-js-object.php
+++ models/class-base-model-js-object.php
@@ -1,52 +1,26 @@
 <?php
-/**
- * The base javascript object model.
- *
- * @author Daryl Lozupone <daryl@actionhook.com>
- * @version 0.1
- * @since WPMVCBase 0.1
- */
+ /*
+This program is free software; you can redistribute it and/or
+modify it under the terms of the GNU General Public License
+as published by the Free Software Foundation; either version 2
+of the License, or (at your option) any later version.
+
+This program is distributed in the hope that it will be useful,
+but WITHOUT ANY WARRANTY; without even the implied warranty of
+MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+GNU General Public License for more details.
+
+You should have received a copy of the GNU General Public License
+along with this program; if not, write to the Free Software
+Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+*/
+
 if ( ! class_exists( 'Base_Model_JS_Object' ) ):
 	/**
 	 * The base javascript object model.
 	 *
-	 * Example:
-	 * <code>
-	 * $script = new Base_Model_JS_Object(
-	 * 		'my-script'
-	 * 		'http://example.com/wp-content/plugins/my-plugin/js/my-script.js',
-	 * 		array( 'jquery', 'jquery-ui' ),
-	 * 		'5.4.17'
-	 * 		false,
-	 * 		'myscriptl10n',
-	 *		array( 'errorMessage' => __( 'There was an error.', 'mytxtdomain')
-	 * );
-	 * </code>
-	 *
-	 * This class exposes two filters, ah_base_filter_script_src-$handle and ah_base_filter_script_localization_args-$handle.
-	 * These are included to allow the plugin controller to modify these elements immediately prior to enqueuing of the script.
-	 *
-	 * Example:
-	 * <code>
-	 * public function filter_script_source( $source )
-	 * {
-	 * 		$source = 'http://example.com/wp-content/plugins/my-plugin/js/my-script.min.js';
-	 * 		return $source;
-	 * }
-	 * add_filter( 'ah_base_filter_script_src-my-script', 'filter_script_source' );
-	 *
-	 * public function filter_script_localization( $args )
-	 * {
-	 * 		$args['my_arg'] = 'foo';
-	 * 		return $args;
-	 * }
-	 * add_filter( 'ah_base_filter_script_localization_args-my-script', 'filter_script_localization' );
-	 * </code>
-	 *
-	 * If using a script already registered by WordPress, you need only specify the handle.
-	 *
 	 * @package WPMVCBase\Models
-	 * @since WPMVCBase 0.1
+	 * @since   WPMVCBase 0.1
 	 */
 	class Base_Model_JS_Object
 	{
@@ -57,8 +31,9 @@ if ( ! class_exists( 'Base_Model_JS_Object' ) ):
 		 * the preceding part of the string refers to the registered handle, and the succeeding part is
 		 * appended to the URL as a query string.
 		 *
-		 * @var string
-		 * @since 0.1
+		 * @var    string
+		 * @access private
+		 * @since  WPMVCBase 0.1
 		 */
 		private $handle;
 
@@ -71,8 +46,9 @@ if ( ! class_exists( 'Base_Model_JS_Object' ) ):
 		 * URL, e.g. //otherdomain.com/js/their-script.js. This parameter is only required when the script with the given
 		 * $handle has not been already registered using wp_register_script().
 		 *
-		 * @var string
-		 * @since 0.1
+		 * @var    string
+		 * @access private
+		 * @since  WPMVCBase 0.1
 		 */
 		private $src;
 
@@ -82,42 +58,49 @@ if ( ! class_exists( 'Base_Model_JS_Object' ) ):
 		 * Array of the handles of all the registered scripts upon which this script depends, that is the scripts that must be
 		 * loaded before this script. Set false if there are no dependencies.
 		 *
-		 * @var array
-		 * @since 0.1
+		 * @var    array
+		 * @access private
+		 * @since  WPMVCBase 0.1
 		 */
 		private $deps;
 
 		/**
 		 * The script version number.
 		 *
-		 * String specifying the script version number, if it has one, which is concatenated to the end of the path as a query string.
-		 * If no version is specified or set to false, then WordPress automatically adds a version number equal to the current version
-		 * of WordPress you are running. If set to null no version is added. This parameter is used to ensure that the correct version
-		 * is sent to the client regardless of caching, and so should be included if a version number is available and makes sense for the script.
+		 * String specifying the script version number, if it has one, which is concatenated to the end of the 
+		 * path as a query string. If no version is specified or set to false, then WordPress automatically adds 
+		 * a version number equal to the current version of WordPress you are running. If set to null no version
+		 * is added. This parameter is used to ensure that the correct version is sent to the client regardless 
+		 * of caching, and so should be included if a version number is available and makes sense for the script.
 		 *
-		 * @var string|bool
-		 * @since 0.1
+		 * @var    string|bool
+		 * @access private
+		 * @since  WPMVCBase 0.1
 		 */
 		private $version;
 
 		/**
 		 * Script placement.
 		 *
-		 * Normally, scripts are placed in <head> of the HTML document. If this parameter is true, the script is placed before the </body> end tag.
+		 * Normally, scripts are placed in <head> of the HTML document. If this parameter is true, 
+		 * the script is placed before the </body> end tag.
 		 *
-		 * @var bool
-		 * @since 0.1
+		 * @var    bool
+		 * @access private
+		 * @since  WPMVCBase 0.1
 		 */
 		private $in_footer;
 
 		/**
 		 * The localization variable name.
 		 *
-		 * String specifying the localization variable name to be used in the wp_localize_script() function. If set, the $localization_args property
-		 * is required and this class will attempt to localize the script immediately after enqueuing the script.
+		 * String specifying the localization variable name to be used in the wp_localize_script() function. 
+		 * If set, the $localization_args property is required and this class will attempt to localize the 
+		 * script immediately after enqueuing the script.
 		 *
-		 * @var string
-		 * @since 0.1
+		 * @var    string
+		 * @access private
+		 * @since  WPMVCBase 0.1
 		 */
 		private $localization_var;
 
@@ -125,24 +108,27 @@ if ( ! class_exists( 'Base_Model_JS_Object' ) ):
 		 * The localization arguments.
 		 *
 		 * An array of key/value pairs containing the argument name(s) and value(s).
-		 * If the $localization_var property is set, this will be used as the $args parameter of the wp_localize_script function.
+		 * If the $localization_var property is set, this will be used as the $args parameter of 
+		 * the wp_localize_script function.
 		 *
-		 * @var array
-		 * @since 0.1
+		 * @var    array
+		 * @access private
+		 * @since  WPMVCBase 0.1
 		 */
 		private $localization_args;
 
 		/**
 		 * The class constructor
 		 *
-		 * @param string $handle
-		 * @param string $src
-		 * @param array $deps
-		 * @param string|bool $version
-		 * @param bool $in_footer
-		 * @param string $localization_var
-		 * @param array $localization_args
-		 * @since 0.1
+		 * @param  string      $handle
+		 * @param  string      $src
+		 * @param  array       $deps
+		 * @param  string|bool $version
+		 * @param  bool        $in_footer
+		 * @param  string      $localization_var
+		 * @param  array       $localization_args
+		 * @access public
+		 * @since  WPMVCBase 0.1
 		 */
 		public function __construct( $handle, $src = false, $deps = array(), $version = false, $in_footer = false, $localization_var = null, $localization_args = null )
 		{
@@ -160,7 +146,7 @@ if ( ! class_exists( 'Base_Model_JS_Object' ) ):
 		 *
 		 * @link http://codex.wordpress.org/Function_Reference/wp_register_script
 		 * @return void
-		 * @since 0.1
+		 * @since WPMVCBase 0.1
 		 */
 		public function register()
 		{
@@ -172,7 +158,7 @@ if ( ! class_exists( 'Base_Model_JS_Object' ) ):
 		 *
 		 * @link http://codex.wordpress.org/Function_Reference/wp_enqueue_script
 		 * @return void
-		 * @since 0.1
+		 * @since WPMVCBase 0.1
 		 */
 		public function enqueue()
 		{
@@ -191,7 +177,7 @@ if ( ! class_exists( 'Base_Model_JS_Object' ) ):
 		 *
 		 * @link http://codex.wordpress.org/Function_Reference/wp_localize_script
 		 * @return bool TRUE on success, FALSE on failure
-		 * @since 0.1
+		 * @since WPMVCBase 0.1
 		 */
 		public function localize()
 		{
@@ -207,7 +193,7 @@ if ( ! class_exists( 'Base_Model_JS_Object' ) ):
 		 *
 		 * @link http://codex.wordpress.org/Function_Reference/wp_dequeue_script
 		 * @return void
-		 * @since 0.1
+		 * @since WPMVCBase 0.1
 		 */
 		public function dequeue()
 		{
@@ -219,7 +205,7 @@ if ( ! class_exists( 'Base_Model_JS_Object' ) ):
 		 *
 		 * @link http://codex.wordpress.org/Function_Reference/wp_deregister_script
 		 * @return void
-		 * @since 0.1
+		 * @since WPMVCBase 0.1
 		 */
 		public function deregister()
 		{
