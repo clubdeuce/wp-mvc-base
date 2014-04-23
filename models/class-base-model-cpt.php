ed484de72799e72f6e6476317d1d65d908f68f51
clubduece <daryl@clubduece.com>
2014-04-23 02:01:47 +0000

    Remove duplicate taxonomy property declaration

--- models/class-base-model-cpt.php
+++ models/class-base-model-cpt.php
@@ -339,15 +339,6 @@ if ( ! class_exists( 'Base_Model_CPT' ) && class_exists( 'Base_Model' ) ):
 		 * @link   http://codex.wordpress.org/Function_Reference/register_post_type
 		 */
 		protected $messages;
-		
-		/**
-		 * The taxonomies attached to this post type.
-		 * 
-		 * @access private
-		 * @var    array
-		 * @since  0.1
-		 */
-		private $taxonomies;
 	
 		/**
 		 * The class constructor.
