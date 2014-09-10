98f4c954271993d24a26a1ee398af1beacb6ca87
clubduece <daryl@clubduece.com>
2014-09-10 11:49:57 +0000

    Change $src parameter default value to null

--- models/class-base-model-js-object.php
+++ models/class-base-model-js-object.php
@@ -130,7 +130,7 @@ if ( ! class_exists( 'Base_Model_JS_Object' ) ):
 		 * @access public
 		 * @since  WPMVCBase 0.1
 		 */
-		public function __construct( $handle, $src = false, $deps = array(), $version = false, $in_footer = false, $localization_var = null, $localization_args = null )
+		public function __construct( $handle, $src = null, $deps = array(), $version = false, $in_footer = false, $localization_var = null, $localization_args = null )
 		{
 			$this->handle            = $handle;
 			$this->src               = $src;
