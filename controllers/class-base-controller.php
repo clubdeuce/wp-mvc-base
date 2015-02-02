e83ceca3595c8d52470276a1af1d08ef7425f6f1
Daryl Lozupone <daryl@clubduece.com>
2015-02-01 22:30:23 -0500

    Apply WP coding standards

--- controllers/class-base-controller.php
+++ controllers/class-base-controller.php
@@ -15,7 +15,7 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-if ( ! class_exists( 'Base_Controller' ) ):
+if ( ! class_exists( 'Base_Controller' ) ) {
 	/**
 	 * The base controller.
 	 *
@@ -207,4 +207,4 @@ if ( ! class_exists( 'Base_Controller' ) ):
 			);
 		}
 	}
-endif;
+}
