fdce1855a8122143ad16668701bc9b754b1bd988
Daryl Lozupone <daryl@clubduece.com>
2015-04-17 16:28:09 -0400

    Refactor main WPMVCB class and post type controller and model bases

--- controllers/class-base-controller-cpt.php
+++ controllers/class-base-controller-cpt.php
@@ -25,7 +25,41 @@ if ( ! class_exists( 'Base_Controller_CPT' ) && class_exists( 'Base_Controller' 
 	 * @since   WPMVCBase 0.2
 	 */
 	class Base_Controller_CPT extends Base_Controller
-	{	
+	{
+		/**
+		 * The post type slug
+		 *
+		 * @var string
+		 */
+		public static $post_type = null;
+
+		/**
+		 * The singular name ( e.g. Book )
+		 *
+		 * @var    string
+		 * @access protected
+		 * @since  WPMVCBase 0.1
+		 */
+		protected static $singular;
+
+		/**
+		 * The plural name ( e.g. Books )
+		 *
+		 * @var    string
+		 * @access protected
+		 * @since  WPMVCBase 0.1
+		 */
+		protected static $plural;
+
+		/**
+		 * The arguments used to register the post type
+		 *
+		 * @var    array
+		 * @access protected
+		 * @since  WPMVCBase 0.4
+		 */
+		protected static $post_type_args = array();
+
 		/**
 		 * The class constructor.
 		 *
@@ -36,21 +70,7 @@ if ( ! class_exists( 'Base_Controller_CPT' ) && class_exists( 'Base_Controller' 
         {
 			parent::__construct( $args );
 
-			add_action( 'init',                  array( $this, 'register' ) );
 			add_filter( 'post_updated_messages', array( $this, 'post_updated_messages' ) );
-		}
-
-		/**
-		 * Register this post type.
-		 *
-		 * @return array An array containing the registered post type objects on success, WP_Error object on failure
-		 * @access public
-		 * @since  WPMVCBase 0.2
-		 * @link   http://codex.wordpress.org/Function_Reference/register_post_type
-		 */
-		public function register()
-		{
-			return register_post_type( $this->model->get_slug(), $this->model->get_args() );
 		}
 
 		/**
@@ -67,25 +87,25 @@ if ( ! class_exists( 'Base_Controller_CPT' ) && class_exists( 'Base_Controller' 
 		{
 			global $post;
 
-			$messages[ $this->model->get_slug() ] = array(
+			$messages[ self::$post_type ] = array(
 				0 => null, // Unused. Messages start at index 1.
 				1 => sprintf(
 					__( '%1$s updated. <a href="%3$s">View %2$s</a>', 'wpmvcb' ),
-					$this->model->get_singular(),
-					strtolower( $this->model->get_singular() ),
+					self::$singular,
+					strtolower( self::$singular ),
 					esc_url( get_permalink( $post->ID ) )
 				),
 				2 => __( 'Custom field updated.', 'wpmvcb' ),
 				3 => __( 'Custom field deleted.', 'wpmvcb' ),
-				4 => sprintf( __( '%s updated.', 'wpmvcb' ), $this->model->get_singular() ),
+				4 => sprintf( __( '%s updated.', 'wpmvcb' ), self::$singular ),
 				/* translators: %2$s: date and time of the revision */
-				5 => isset( $_GET['revision'] ) ? sprintf( __( '%1$s restored to revision from %s', 'wpmvcb' ), $this->model->get_singular(), wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
-				6 => sprintf( __( '%1$s published. <a href="%2$s">View %1$s</a>', 'wpmvcb' ), $this->model->get_singular(), esc_url( get_permalink( $post->ID ) ) ),
-				7 => sprintf( __( '%s saved.', 'wpmvcb' ), $this->model->get_singular() ),
+				5 => isset( $_GET['revision'] ) ? sprintf( __( '%1$s restored to revision from %s', 'wpmvcb' ), self::$singular, wp_post_revision_title( (int) $_GET['revision'], false ) ) : false,
+				6 => sprintf( __( '%1$s published. <a href="%2$s">View %1$s</a>', 'wpmvcb' ), self::$singular, esc_url( get_permalink( $post->ID ) ) ),
+				7 => sprintf( __( '%s saved.', 'wpmvcb' ), self::$singular ),
 				8 => sprintf(
 					__( '%1$s submitted. <a target="_blank" href="%3$s">Preview %2$s</a>', 'wpmvcb' ),
-					$this->model->get_singular(),
-					strtolower( $this->model->get_singular() ),
+					self::$singular,
+					strtolower( self::$singular ),
 					esc_url( add_query_arg( 'preview', 'true', get_permalink( $post->ID ) ) )
 				),
 				9 => sprintf(
@@ -93,18 +113,45 @@ if ( ! class_exists( 'Base_Controller_CPT' ) && class_exists( 'Base_Controller' 
 					// translators: Publish box date format, see http://php.net/date
 					date_i18n( __( 'M j, Y @ G:i' ), strtotime( $post->post_date ) ),
 					esc_url( get_permalink( $post->ID ) ),
-					$this->model->get_singular(),
-					strtolower( $this->model->get_singular() )
+					self::$singular,
+					strtolower( self::$singular )
 				),
 				10 => sprintf(
 					__( '%1$s draft updated. <a target="_blank" href="%3$s">Preview %2$s</a>', 'wpmvcb' ),
-					$this->model->get_singular(),
-					strtolower( $this->model->get_singular() ),
+					self::$singular,
+					strtolower( self::$singular ),
 					esc_url( add_query_arg( 'preview', 'true', get_permalink( $post->ID ) ) )
 				)
 			);
 
 			return $messages;
 		}
+
+		/**
+		 * Initialize the CPT labels property.
+		 *
+		 * @access protected
+		 * @since  WPMVCBase 0.1
+		 */
+		protected static function init_labels( $singular = __CLASS__, $plural = __CLASS__ )
+		{
+			return array(
+				'name'                => $plural,
+				'singular_name'       => $singular,
+				'menu_name'           => $plural,
+				'parent_item_colon'   => sprintf( __( 'Parent %s', 'wpmvcb' ), $singular ),
+				'all_items'           => sprintf( __( 'All %s', 'wpmvcb' ), $plural ),
+				'view_item'           => sprintf( __( 'View %s', 'wpmvcb' ), $singular ),
+				'add_new_item'        => sprintf( __( 'Add New %s', 'wpmvcb' ), $singular ),
+				'add_new'             => sprintf( __( 'New %s', 'wpmvcb' ), $singular ),
+				'edit_item'           => sprintf( __( 'Edit %s', 'wpmvcb' ), $singular ),
+				'update_item'         => sprintf( __( 'Update %s', 'wpmvcb' ), $singular ),
+				'search_items'        => sprintf( __( 'Search %s', 'wpmvcb' ), $plural ),
+				'not_found'           => sprintf( __( 'No %s found', 'wpmvcb' ), strtolower( $plural ) ),
+				'not_found_in_trash'  => sprintf( __( 'No %s found in Trash', 'wpmvcb' ), strtolower( $plural ) ),
+			);
+		}
+
 	}
+
 }
--- wpmvcb.php
+++ wpmvcb.php
@@ -15,48 +15,70 @@ along with this program; if not, write to the Free Software
 Foundation, Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
 */
 
-class WPMVCB
-{
+class WPMVCB  {
+
+    /**
+     * A list of files that must be loaded at plugin initialization
+     *
+     * @var array
+     */
+    private $mustload_files = array();
+
     /**
      * A list of all classes contained in the framework.
      *
      * @var array
      */
-    private static $autoload_classes;
+    private $autoload_classes = array();
+
+    /**
+     * A collection of post type slugs and arguments used to register the post type as key/value pairs
+     *
+     * @var array
+     */
+    private $post_type_args = array();
 
     /**
      * Initialize the class
      *
      * @return void
      */
-    public static function init() {
-        self::$autoload_classes = array(
-            'Base_Controller'             => 'controllers/class-base-controller.php',
-            'Base_Controller_CPT'         => 'controllers/class-base-controller-cpt.php',
-            'Base_Controller_Plugin'      => 'controllers/class-base-controller-plugin.php',
-            'Base_Controller_Settings'    => 'controllers/class-base-controller-settings.php',
-            'WPMVCB_Metabox'              => 'controllers/class-metabox-base.php',
-            'Base_Model'                  => 'models/class-base-model.php',
-            'Base_Model-Admin-Notice'     => 'models/class-base-model-admin-notice.php',
-            'Base_Model_CPT'              => 'models/class-base-model-cpt.php',
-            'Base_Model_Help_Tab'         => 'models/class-base-model-help-tab.php',
-            'Base_Model_JS_Object'        => 'models/class-base-model-js-object.php',
-            'Base_Model_Menu_Page'        => 'models/class-base-model-menu-page.php',
-            'Base_Model_Metabox'          => 'models/class-base-model-metabox.php',
-            'Base_Model_Plugin'           => 'models/class-base-model-plugin.php',
-            'Base_Model_Settings'         => 'models/class-base-model-settings.php',
-            'Base_Model_Taxonomy'         => 'models/class-base-model-taxonomy.php',
-            'WPMVCB_View_Base'            => 'views/class-view-base.php',
-            'WPMVCB_Metabox_View_Base'    => 'views/class-metabox-view-base.php',
-            'WPMVCB_Metabox_Default_View' => 'views/class-metabox-view-default.php',
-            'Helper_Functions'            => 'helpers/class-base-helpers.php',
+    public function __construct() {
+        $this->autoload_classes = array(
+            'Base_Controller'             => __DIR__ . '/controllers/class-base-controller.php',
+            'Base_Controller_CPT'         => __DIR__ . '/controllers/class-base-controller-cpt.php',
+            'Base_Controller_Plugin'      => __DIR__ . '/controllers/class-base-controller-plugin.php',
+            'Base_Controller_Settings'    => __DIR__ . '/controllers/class-base-controller-settings.php',
+            'WPMVCB_Metabox'              => __DIR__ . '/controllers/class-metabox-base.php',
+            'Base_Model'                  => __DIR__ . '/models/class-base-model.php',
+            'Base_Model-Admin-Notice'     => __DIR__ . '/models/class-base-model-admin-notice.php',
+            'Base_Model_CPT'              => __DIR__ . '/models/class-base-model-cpt.php',
+            'Base_Model_Help_Tab'         => __DIR__ . '/models/class-base-model-help-tab.php',
+            'Base_Model_JS_Object'        => __DIR__ . '/models/class-base-model-js-object.php',
+            'Base_Model_Menu_Page'        => __DIR__ . '/models/class-base-model-menu-page.php',
+            'Base_Model_Metabox'          => __DIR__ . '/models/class-base-model-metabox.php',
+            'Base_Model_Plugin'           => __DIR__ . '/models/class-base-model-plugin.php',
+            'Base_Model_Settings'         => __DIR__ . '/models/class-base-model-settings.php',
+            'Base_Model_Taxonomy'         => __DIR__ . '/models/class-base-model-taxonomy.php',
+            'WPMVCB_View_Base'            => __DIR__ . '/views/class-view-base.php',
+            'WPMVCB_Metabox_View_Base'    => __DIR__ . '/views/class-metabox-view-base.php',
+            'WPMVCB_Metabox_Default_View' => __DIR__ . '/views/class-metabox-view-default.php',
+            'Helper_Functions'            => __DIR__ . '/helpers/class-base-helpers.php',
         );
 
-        spl_autoload_register( array( __CLASS__, 'autoloader' ) );
+        spl_autoload_register( array( $this, 'autoloader' ) );
+
+        add_action( 'init', array( $this, 'init' ) );
+        add_action( 'muplugins_loaded', array( $this, 'load_mustloads' ) );
     }
 
-    public static function register_autoload_class( array $classes ) {
-        self::$autoload_classes = array_merge( self::$autoload_classes, $classes );
+    /**
+     * Add additional class name/absloute path pairs to the list of classes able to be autoloaded.
+     *
+     * @param array $classes
+     */
+    public function register_autoload_classes( array $classes ) {
+        $this->autoload_classes = array_merge( $this->autoload_classes, $classes );
     }
 
     /**
@@ -65,12 +87,56 @@ class WPMVCB
      * $param string $class The class to be loaded
      * @return void
      */
-    public static function autoloader( $class ) {
-        if ( isset( self::$autoload_classes[ $$class ] ) ) {
-            if ( file_exists( self::$autoload_classes[ $class ] ) ) {
-                require_once self::$autoload_classes[ $class ];
+    public function autoloader( $class ) {
+        if ( isset( $this->autoload_classes[ $class ] ) ) {
+            if ( file_exists( $this->autoload_classes[ $class ] ) ) {
+                require_once $this->autoload_classes[ $class ];
             }
         }
+    }
+
+    /**
+     * Add files that must be loaded at plugin initialization
+     *
+     * @param array $files
+     */
+    public function register_mustload_files( array $files ) {
+
+        $this->mustload_files = array_merge( $files, $this->mustload_files );
+
+    }
+
+    /**
+     * Load the files in the $mustload property
+     */
+    public function load_mustloads() {
+
+        foreach( $this->mustload_files as $file ) {
+            if( file_exists( $file ) ) {
+                require_once $file;
+            }
+        }
+
+    }
+
+    public function register_post_type_args( $slug, $args = array() ) {
+
+        $this->post_type_args[ $slug ] = $args;
+
+    }
+
+    public function init() {
+
+        $this->register_post_types();
+
+    }
+
+    private function register_post_types() {
+
+        foreach( $this->post_type_args as $slug => $args ) {
+            register_post_type( $slug, $args );
+        }
+        
     }
 
     /**
@@ -84,5 +150,3 @@ class WPMVCB
 
     }
 }
-
-WPMVCB::init();
